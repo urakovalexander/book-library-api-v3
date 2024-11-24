871df397abbaa354e8df07c04fbe3b586d3264e3
urakovalexander <urakov.alexander@gmail.com>
2024-11-24 20:11:51 +0500

    Configure JWT authentication with environment variables

---
+++ config/jwt.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'secret_key' => $_ENV['JWT_SECRET_KEY'],
+    'issuer' => $_ENV['JWT_ISSUER'],
+    'audience' => $_ENV['JWT_AUDIENCE'],
+    'expiration_time' => $_ENV['JWT_EXPIRATION_TIME'],
+    'algorithm' => 'HS256',
+];
